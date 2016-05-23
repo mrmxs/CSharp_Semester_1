d66e4638e56f2e7a1f729d9b8aa2d77e23b03e0b
mrmxs <maryzmaxs@gmail.com>
2016-05-24 02:43:29 +0300

    Add Task#1 of 3 in Lab2

---
+++ Lab2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Lab2
+{
+    class Program
+    {
+        public static string Funct(short fNum, float x)
+        {
+            switch (fNum)
+            {
+                case 1:
+                    Console.Write("x + 2 * x * x - 3 = ");
+                    return (x + 2 * x * x - 3).ToString();
+                case 2:
+                    Console.Write("x * x * x – 4 * x = ");
+                    return (x * x * x - 4 * x).ToString();
+                case 3:
+                    Console.Write("3 + 2 * x * x – 4 * x * x * x = ");
+                    return (3 + 2 * x * x - 4 * x * x * x).ToString();
+                case 4:
+                    Console.Write("6 * x – x – 4 * x * x = ");
+                    return (6 * x - x - 4 * x * x).ToString();
+                case 5:
+                    Console.Write("3 – x + 2 * x * x * x = ");
+                    return (3 - x + 2 * x * x * x).ToString();
+                case 6:
+                    Console.Write("4 * x * x + 3 * x – 5 = ");
+                    return (4 * x * x + 3 * x - 5).ToString();
+                case 7:
+                    Console.Write("2 * x – 3 * x * x * x + 3 = ");
+                    return (2 * x - 3 * x * x * x + 3).ToString();
+                case 8:
+                    Console.Write("3 * x * x + 4 * x * x – 2 * x + 1 = ");
+                    return (3 * x * x + 4 * x * x - 2 * x + 1).ToString();
+                case 9:
+                    Console.Write("2 * x + 4 – 5 * x * x = ");
+                    return (2 * x + 4 - 5 * x * x).ToString();
+                case 10:
+                    Console.Write("4 * x + 3 * x * x - 5 = ");
+                    return (4 * x + 3 * x * x - 5).ToString();
+            }
+            return "function not exist";
+        }
+
+
+        static void Main(string[] args)
+        {
+            #region task 1
+            Console.WriteLine("task1\nРассчитать значение функции в заданной точке.");
+            Console.WriteLine("Введите значение x.");
+            float x = Convert.ToSingle(Console.ReadLine());
+            for (short i = 1; i < 10; i++)
+            {
+                Console.WriteLine(Funct(i, x));
+            }
+            Console.WriteLine("press for the next");
+            Console.ReadKey();
+            #endregion
+
+
+
+
+        }
+    }
+}
